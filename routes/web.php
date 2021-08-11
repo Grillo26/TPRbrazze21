511e1d164bd3b86b3556edb22c273274d907508d
Carlos Enrique Mamani <carlosenriquetorrez26@gmail.com>
2021-08-10 23:51:12 -0400

    create controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,28 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    /*return view('welcome');*/
-    return "Bienvenido a la pagina principal";
-});
+Route::get('/', HomeController::class);
+Route::get('cursos', [CursoController::class, 'index']);
+Route::get('cursos/create', [CursoController::class, 'create']);
+Route::get('cursos/{curso}', [CursoController::class, 'show']);
 
-Route::get('cursos', function(){
-    return "Bienvenido a la pagina de cursos";
-});
 
-Route::get('cursos/create', function () {
-    return "En esta pagina podras crear un curso";
-});
-
-Route::get('cursos/{curso}', function($curso){
-    return "Bienvenido al curso : $curso";
-});
-
-Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria= null) {
-    if($categoria){
-        return "Bienvenido al curso: $curso, de la categoria: $categoria";
-    }
-    else{
-        return "Lo que sea";
-    }
-});
