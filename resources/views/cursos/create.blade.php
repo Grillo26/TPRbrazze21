428827c3ee38fb7092a43bd6af5395e659f17290
Carlos Enrique Mamani <carlosenriquetorrez26@gmail.com>
2021-08-19 21:26:56 -0400

    Creación de vistas

---
+++ resources/views/cursos/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title', 'CursosCreate')
+    
+@section('content')
+    <h1>En esta pagina podras crear cursos</h1>
+@endsection
---
+++ resources/views/cursos/show.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Curso'. $curso)
+    
+@section('content')
+    <h1>Bienvenido al Curso {{$curso}}</h1>
+@endsection
